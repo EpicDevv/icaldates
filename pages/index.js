653a0cbef18485eb34278de229ca530447d488b0
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-04-11 01:21:39 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
   const event = {
     title: "My Title",
     description: "My Description",
-    startTime: "2018-10-07T10:30:00+10:00",
-    endTime: "2018-10-07T12:00:00+10:00",
+    startTime: "2022-04-12T10:30:00+10:00",
+    endTime: "2022-04-15T12:00:00+10:00",
     location: "10 Carlotta St, Artarmon NSW 2064, Australia",
     attendees: [
       "Hello World <hello@world.com>",
